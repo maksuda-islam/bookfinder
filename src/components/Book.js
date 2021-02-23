c93c8b87bb77ba6571acacc1efe6e7ddcbe0a5da
Maksuda Islam Lima <maksudaislam@iut-dhaka.edu>
2021-02-24 01:03:01 +0600

    card upadted

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,10 +3,8 @@ import { Link } from "react-router-dom";
 import Masonry from 'react-masonry-css';
 const Book = (props) => {
     const breakpointColumnsObj = {
-        default: 4,
-        1100: 3,
-        700: 2,
-        500: 1
+        default:  4, 
+        800: 2
       };
     return (
         <Masonry
@@ -31,17 +29,17 @@ const Book = (props) => {
                             />
                         )}
 
-                    <span className="card-header"><h7 className="my-masonry-grid_column">{props.data.volumeInfo.title}</h7>
+                    <span className="card-title">{props.data.volumeInfo.title}
                     </span>
                 </div>
-                    <div className="card-body">
+                    <div className="card-content">
                     {props.data.volumeInfo.authors == undefined ? (
                         <p><h7>No Author Infos</h7></p>
                     ) : (
                        <p> {props.data.volumeInfo.authors[0]} </p>
                     )}
                     </div>
-                <div className="app-card-list" id="app-card-list">
+                <div className="card-action">
                     <Link to={{pathname: "/book/" + props.data, title: props.data.volumeInfo.title ,  
                     subtitle: props.data.volumeInfo.subtitle, publisher: props.data.volumeInfo.publisher , publishedDate:props.data.volumeInfo.publishedDate, description:props.data.volumeInfo.description  }}>See Details</Link>
                     
